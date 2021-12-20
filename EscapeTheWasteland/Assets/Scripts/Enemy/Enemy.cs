c3da90d4650a10ba700fc8d6ef60d4cc139b96c2
Shay Sedaka <shays@justplay.lol>
2021-12-20 16:18:23 +0200

    Enemies can now detect players in radius

---
+++ EscapeTheWasteland/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public enum EnemyState
+{
+    IDLE,
+    COMBAT
+}
+
+public class Enemy : MonoBehaviour
+{ 
+
+    [SerializeField] int _hp = 5;
+    [SerializeField] float _fireRateInSeconds = 0.6f;
+    [SerializeField] float _detectionRadius = 20f;
+
+    [SerializeField] GameObject _alertEffect;
+
+    private EnemyState _currentState;
+    private GameObject _detectedPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentState = EnemyState.IDLE;
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, _detectionRadius);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateEnemyState();
+
+        switch (_currentState)
+        {
+            case EnemyState.IDLE:
+                OnIdle();
+                break;
+
+            case EnemyState.COMBAT:
+                OnCombat();
+                break;
+        }        
+    }
+
+
+    private void RotateEnemy(Vector3 direction)
+    {
+        float angle = Mathf.Atan2(-direction.x, direction.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+
+
+    private void OnCombat()
+    {
+        //get pure shot direction by vector subtraction
+        _alertEffect.SetActive(true);
+        Vector3 shotDirection = (_detectedPlayer.transform.position - gameObject.transform.position).normalized;
+        RotateEnemy(shotDirection);
+    }
+
+    private void OnIdle()
+    {
+        _alertEffect.SetActive(false);
+        
+    }
+
+    private void UpdateEnemyState()
+    {
+
+        bool _wasPlayerSpotted = false;
+
+        Collider2D[] _collidersWithinRange = Physics2D.OverlapCircleAll(transform.position, _detectionRadius);
+        foreach (Collider2D col in _collidersWithinRange)
+        {
+            if (col.tag.Equals("Player"))
+            {
+                _detectedPlayer = col.gameObject;
+                _wasPlayerSpotted = true;
+                break;
+            }
+        }
+
+        if (_wasPlayerSpotted)
+        {
+            _currentState = EnemyState.COMBAT;
+        }
+        else
+        {
+            _currentState = EnemyState.IDLE;
+        }
+    }
+
+}
